73cd2c54d59df70e9e5755641874153818ce9467
wuzhenhong <wuzhenhong@banksteel.com>
2024-12-24 11:43:51 +0800

    -

---
+++ src/test/java/com/example/demo/jackson2/ResponseBodyAdviceImpl2.java
@@ -0,0 +1,35 @@
+package com.example.demo.jackson2;
+
+import com.jackson.ThreadLocalUtil;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author wuzhenhong
+ * @date 2024/12/12 8:22
+ */
+@ControllerAdvice()
+public class ResponseBodyAdviceImpl2 implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+        Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
+        ServerHttpResponse response) {
+
+        ThreadLocalUtil.set(returnType);
+        return body;
+    }
+
+}
