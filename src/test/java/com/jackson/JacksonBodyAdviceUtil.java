896db46a0343efa3f032c4f288b21e0b66538aee
wuzhenhong <wuzhenhong@banksteel.com>
2024-12-24 11:37:04 +0800

    jackson处理时间

--- src/test/java/com/jackson/JacksonBodyAdviceUtil.java
+++ src/test/java/com/jackson/JacksonBodyAdviceUtil.java
@@ -3,6 +3,14 @@ package com.jackson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author wuzhenhong
@@ -13,9 +21,18 @@ public class JacksonBodyAdviceUtil {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     static {
+        OBJECT_MAPPER.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
         SimpleModule module = new SimpleModule();
         // 然后这个传方法值，可以通过线程上下文传过去，这里只是demo就这么写了，得优化的
         module.addSerializer(String.class, new CustomStringSerializer());
+        module.addDeserializer(LocalDate.class,
+            new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        module.addDeserializer(LocalDateTime.class,
+                new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+            // 日期序列化，主要返回数据时使用
+        module.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        module.addSerializer(LocalDateTime.class,
+                new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         OBJECT_MAPPER.registerModule(module);
     }
 
